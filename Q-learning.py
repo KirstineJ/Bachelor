a8c77c2da317c4059c2f54fcc012bc86de52822e
KirstineJ <118969487+KirstineJ@users.noreply.github.com>
2024-02-09 13:31:05 +0100

    Update Q-learning.py
    Fredag d. 9.februar kl.13:30

--- Q-learning.py
+++ Q-learning.py
@@ -5,6 +5,9 @@ import time
 import multiprocessing as mp
 from matplotlib import pyplot as plt
 
+#np.random.seed(321)
+np.random.seed(981)
+
 def Demand(p1, p2):
     if p1 < p2:
         return (1 - p1)
@@ -26,12 +29,10 @@ def Qfunction(price, period, delta, alpha, theta):
     price1 = int(np.random.choice(len(price))) # index i matrix kan ikke være en float
     price2 = int(np.random.choice(len(price)))
 
-    price_s = int(np.random.choice(len(price)))
+    state = int(np.random.choice(len(price)))
 
     Qtable_1 = np.zeros((len(price), len(price)))
     Qtable_2 = np.zeros((len(price), len(price)))
-
-    epsilon = (1 - theta)**period
 
 
     t = 3
@@ -39,49 +40,87 @@ def Qfunction(price, period, delta, alpha, theta):
     j = 2
 
     for t in range(t, period + 1):
-        if (t % 2) == 0:
+        epsilon = (1 - theta)**t
+        
+        if (t % 2) != 0:
+            print("Firm 1\n")
+
             # Previous estimate
-            prev_estimate = Qtable_1[price1, price_s]
+            prev_estimate = Qtable_1[price1, price2]
+            print("price1:\n", price1)
+            print("price2:\n", price2)
+            print("prev_estimate:\n", prev_estimate)
 
             # New estimate
-            profit_current_state = Profit(price1, price2)
-            profit_next_state = delta * Profit(price1, price_s)
+            profit_current_state = Profit(price[price1], price[price2])
+            print("profit_current_state:\n", profit_current_state)
 
-            max_Q = np.argmax(Qtable_1[:, price_s])
+            profit_next_state = delta * Profit(price[price1], price[state])
+            print("state:\n", state)
+            print("profit_next_state:\n", profit_next_state)
+
+            max_Q = np.argmax(Qtable_1[:, state])
+            print("Qtable_1:\n", Qtable_1[:, state])
+            print("Max_Q:\n", max_Q)
+
             max_Q_next_state = delta**2 * max_Q
+            print("max_Q_next_state:\n", max_Q_next_state)
 
             new_estimate = profit_current_state + profit_next_state + max_Q_next_state
+            print("new_estimate:\n", new_estimate)
 
             # Update
             Qtable_1[price1, price2] = (1 - alpha) * prev_estimate + alpha * new_estimate
+            print("Qtable_1:\n", Qtable_1)
+
+            # Set p_it
+            if np.random.uniform(0,1) < epsilon:
+                price1 = int(np.random.choice(len(price)))
+                print("Random\n")
+            else:
+                price1 = np.argmax(Qtable_1[:, state])
+                print("Max\n")
+            
+            print("pris1:\n", price1)
 
         else:
+            print("Firm 2\n")
             # Previous estimate
-            prev_estimate = Qtable_2[price2, price_s]
+            prev_estimate = Qtable_2[price2, state]
+            print("price2:\n", price2)
+            print("state:\n", state)
+            print("prev_estimate:\n", prev_estimate)
 
             # New estimate
-            profit_current_state = Profit(price2, price1)
-            profit_next_state = delta * Profit(price2, price_s)
+            profit_current_state = Profit(price[price2], price[price1])
+            print("price1:\n", price1)
+            print("profit_current_state:\n", profit_current_state)
 
-            max_Q = np.argmax(Qtable_2[:, price_s])
+            profit_next_state = delta * Profit(price[price2], price[state])
+            print("profit_next_state:\n", profit_next_state)
+
+            max_Q = np.argmax(Qtable_2[:, state])
+            print("Qtable_2:\n", Qtable_2[:, state])
+            print("Max_Q:\n", max_Q)
+
             max_Q_next_state = delta**2 * max_Q
+            print("max_Q_next_state:\n", max_Q_next_state)
 
             new_estimate = profit_current_state + profit_next_state + max_Q_next_state
+            print("new_estimate:\n", new_estimate)
 
             # Update
-            Qtable_2[price2, price1] = (1 - alpha) * prev_estimate + alpha * new_estimate # måske skal priserne byttes om
-        
-        # Set p_it and p_jt
-        if np.random.uniform(0,1) < epsilon:
-            price1 = int(np.random.choice(len(price)))
-        else:
-            price1 = np.argmax(Qtable_1[:, price_s])
+            Qtable_2[price2, state] = (1 - alpha) * prev_estimate + alpha * new_estimate 
+            print("Qtable_2:\n", Qtable_2)
 
-        if np.random.uniform(0,1) < epsilon:
-            price2 = int(np.random.choice(len(price)))
-        else:
-            price2 = Qtable_2[price2, price_s]
-        
+            # Set p_jt
+            if np.random.uniform(0,1) < epsilon:
+                price2 = int(np.random.choice(len(price)))
+                print("Random:\n")
+            else:
+                price2 = Qtable_2[price2, state]
+                print("Max\n")
+            print("pris2:\n", price2)
 
         # Update t, i and j
         t = t + 1
@@ -90,9 +129,13 @@ def Qfunction(price, period, delta, alpha, theta):
    
     return Qtable_1, Qtable_2, price1, price2
 
+
+prev_p = np.zeros((2,2), dtype=int)
+prev_p[0,0] = np.random.choice(len(P))
+
+print(prev_p)
     
-    
-print(Qfunction(P, 100, 0.95, 0.3, 0.00002763))
+#print(Qfunction(P, 5, 0.95, 0.3, 0.00002763))
 
         
 
